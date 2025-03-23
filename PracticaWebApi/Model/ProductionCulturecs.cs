0d4cb69b408016960dfa74df7f0f6dbb89ac6562
Elmoestaemputado10 <jonthanalfredo888@gmail.com>
2025-03-22 21:03:00 -0600

    Agregar archivos de proyecto.

---
+++ PracticaWebApi/Model/ProductionCulturecs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+
+namespace PracticaWebApi.Model
+{
+    public class ProductionCulturecs
+    {
+        
+    
+        [Table("Culture", Schema = "Production")]
+        public class Culture
+        {
+            [Key]
+            [Column(TypeName = "nchar(6)")]
+            public string CultureID { get; set; }
+
+            [Required]
+            [Column(TypeName = "nvarchar(50)")]
+            public string Name { get; set; }
+
+            [Required]
+            public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+        }
+
+        
+    }
+}
+
